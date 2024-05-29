86c0bb17a93f139831f4d5f7758247a1ba90f8ba
mprst128 <provostmariecamille12@gmail.com>
2024-05-29 16:40:36 +0200

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   url_api: 'http://esiea-spotymike.eu-4.evennode.com',
+  firebase : {
+    apiKey: "AIzaSyAuM-HDZ9Xk23Z6ra87USG9R5kqkM0Gj94",
+    authDomain: "spotymike-2961a.firebaseapp.com",
+    projectId: "spotymike-2961a",
+    storageBucket: "spotymike-2961a.appspot.com",
+    messagingSenderId: "72006215134",
+    appId: "1:72006215134:web:53c7adf4807def868da94f",
+    measurementId: "G-SB6ZXK6GGJ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,15 @@
 export const environment = {
   production: false,
   url_api: 'http://esiea-spotymike.eu-4.evennode.com/v1',
+  firebase : {
+    apiKey: "AIzaSyAuM-HDZ9Xk23Z6ra87USG9R5kqkM0Gj94",
+    authDomain: "spotymike-2961a.firebaseapp.com",
+    projectId: "spotymike-2961a",
+    storageBucket: "spotymike-2961a.appspot.com",
+    messagingSenderId: "72006215134",
+    appId: "1:72006215134:web:53c7adf4807def868da94f",
+    measurementId: "G-SB6ZXK6GGJ"
+}
 };
  
 /*
