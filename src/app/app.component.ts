6b7bbb5f6a5e8f0bdf6978015c7defaaf0a55256
HajarLaatioui <laatioui@et.esiea.fr>
2024-05-29 10:43:47 +0200

    not finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonButtons, IonButton, IonItem, IonInput, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonContent, IonInput, IonItem, IonButton, IonButtons, IonToolbar, IonHeader, IonApp, IonRouterOutlet],
 })
 export class AppComponent {
   constructor() {}
---
+++ src/app/core/models/refData.ts
@@ -0,0 +1,1 @@
+export enum EAuthPage { 'Login', 'Register'}
