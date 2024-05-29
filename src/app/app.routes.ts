1a3eb8610899070aac90aba16af129b2624a4ffe
mprst128 <provostmariecamille12@gmail.com>
2024-05-29 12:11:04 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,28 @@
 import { Routes } from '@angular/router';
-
+ 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    // Delete
+    path: 'auth',
+    loadComponent: () =>
+      import('./layouts/auth/auth.page').then((m) => m.AuthPage),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./pages/login/login.page').then((m) => m.LoginPage),
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./pages/register/register.page').then((m) => m.RegisterPage),
+      },
+    ],
   },
+
   {
     path: '',
-    redirectTo: 'home',
     pathMatch: 'full',
+    redirectTo: 'auth/login',
   },
 ];
