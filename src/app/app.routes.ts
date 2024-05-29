812a3c03d4abf1f5c9fd6c7d6f634fbe19039861
HajarLaatioui <laatioui@et.esiea.fr>
2024-05-29 12:09:40 +0200

    not finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,31 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    // Delete
+    path: 'auth',
+    loadComponent: () =>
+      import('./layouts/auth/auth.page').then((m) => m.AuthPage),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./pages/login/login.page').then((m) => m.LoginPage),
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./pages/register/register.page').then((m) => m.RegisterPage),
+      },
+    ],
+  },
+  {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () =>
+      import('./shared/tabs/tabs.page').then((m) => m.TabsPage),
   },
   {
     path: '',
-    redirectTo: 'home',
     pathMatch: 'full',
+    redirectTo: 'auth/login',
   },
 ];
---
+++ src/app/pages/tab2/tab2.page.ts
@@ -0,0 +1,15 @@
+import { ExploreContainerComponent } from './../../explore-container/explore-container.component';
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+})
+export class Tab2Page {
+  constructor() {}
+}
