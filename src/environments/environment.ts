31acff15f2b054f8c6e59811b1f87b9f350c1182
mprst128 <provostmariecamille12@gmail.com>
2024-06-07 18:29:02 +0200

    rebase in register

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 // This file can be replaced during build by using the `fileReplacements` array.
@@ -18,16 +17,6 @@ export const environment = {
   }
 }
  
-=======
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false
-};
-
->>>>>>> 108c280 (first commit)
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -35,8 +24,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-<<<<<<< HEAD
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
-=======
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
->>>>>>> 108c280 (first commit)
